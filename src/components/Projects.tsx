cdf1e7d2de5b71593fbd2e0643f2bb6a350bca9d
Kanishq Dhangar <dhangarkanishq@gmail.com>
2026-01-08 16:35:53 +0530

    Resolved Get Hired

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,7 +14,7 @@ const projects = [
   {
     title: "Get Hired",
     description:
-      "GetHired is an AI-powered recruitment platform designed to connect students with the most relevant internship opportunities. By utilizing a FastAPI backend and a Next.js frontend, the application provides a high-performance, real-time recommendation system based on a student’s unique skill profile and career goals.",
+      "GetHired is an AI-powered recruitment platform designed to connect students with the most relevant internship opportunities. The application provides a high-performance, real-time recommendation system based on a student’s unique skill profile and career goals.",
     tech: "Next.js, TypeScript, Tailwind CSS, FastAPI, PostgreSQL",
     image: "/GetHired.png",
     github: "https://github.com/kanishqdhangar/GetHired",
